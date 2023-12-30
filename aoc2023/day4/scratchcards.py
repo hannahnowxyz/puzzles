b3828a507ef067417d29cd86777e94cc3859f317
hannahnowxyz <127896961+hannahnowxyz@users.noreply.github.com>
2023-12-29 21:43:37 -0600

    start day 5

--- aoc2023/day4/scratchcards.py
+++ aoc2023/day4/scratchcards.py
@@ -3,11 +3,11 @@
 input = open("input.txt").readlines()
 win_sum = 0
 for line in input:
-    #extract numbers and hash, no need to parse to int
+    #extract numbers and hash as set, no need to parse to int
     data_part = line.split(":")[1]
     winning_nums = set(data_part.split("|")[0].split())
     pow2 = -1
-    for scratch_num in (data_part.split("|")[1]).split():
+    for scratch_num in data_part.split("|")[1].split():
         pow2 += scratch_num in winning_nums
     #if no scratch numbers win, this adds int(0.5) = 0
     win_sum += int(2**pow2)
---
+++ aoc2023/day4/scratchcards2.py
@@ -0,0 +1,18 @@
+#//https://adventofcode.com/2023/day/4
+
+#assign each card a multiplier for how many copies we have of it
+input = [[1, line] for line in open("input.txt").readlines()]
+num_cards = 0
+for i, [multiplier, line] in enumerate(input):
+    #extract numbers and hash as set, no need to parse to int
+    data_part = line.split(":")[1]
+    winning_nums = set(data_part.split("|")[0].split())
+    win_count = 0
+    for scratch_num in data_part.split("|")[1].split():
+        win_count += scratch_num in winning_nums
+    #add multiplier to next few cards,
+    #because we win one copy of each for each winning copy of this card
+    for j in range(win_count):
+        input[min(i + j + 1, len(input) - 1)][0] += multiplier
+    num_cards += multiplier
+print(num_cards)
