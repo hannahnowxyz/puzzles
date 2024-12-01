0385d89582f5ab32c86e7371117de8183a2bda5c
hannah <127896961+hannahnowxyz@users.noreply.github.com>
2024-12-01 13:41:03 -0600

    aoc2024 day01

--- aoc2024/day01/hysteria.c
+++ aoc2024/day01/hysteria.c
@@ -20,18 +20,20 @@ int main(int argc, char* argv[])
     uint64_t list_a[num_lines];
     uint64_t list_b[num_lines];
     for (size_t i = 0; i < num_lines; i++) {
-	sscanf(lines[i].cstr_ptr, "%" SCNu64 "%" SCNu64, &list_a[i], &list_b[i]);
+        sscanf(lines[i].cstr_ptr, "%" SCNu64 "%" SCNu64, &list_a[i], &list_b[i]);
     }
     qsort(list_a, num_lines, sizeof(list_a[0]), u64_cmp);
     qsort(list_b, num_lines, sizeof(list_b[0]), u64_cmp);
 
     uint64_t total_distance = 0;
     for (size_t i = 0; i < num_lines; i++) {
-	total_distance += abs(list_a[i] - list_b[i]);
+        uint64_t a = list_a[i]; uint64_t b = list_b[i];
+        if (a > b) total_distance += a - b;
+        else total_distance += b - a;
     }
 
     free(lines);
     free(input);
-    printf("solution: %d\n", total_distance);
+    printf("solution: %" PRIu64 "\n", total_distance);
     return 0;
 }
